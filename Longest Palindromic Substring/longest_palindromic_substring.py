b081abfd51bd29c97aaeb8d7041037d9f534fae7
Ishan Gote <goteishan@gmail.com>
2019-03-27 23:23:39 -0400

    returning the longest palindromic substring

--- Longest Palindromic Substring/longest_palindromic_substring.py
+++ Longest Palindromic Substring/longest_palindromic_substring.py
@@ -15,37 +15,44 @@ a  b  a  a  b  c
 import unittest
 def longest_palindromic_substring(input_string):
     max_length = 0
+    longest_palin = ''
     for idx in range(len(input_string)):
         #Odd expansion
-        max_odd_len = get_max_palin_substr(input_string, idx, idx)
+        odd_palin, max_odd_len = get_max_palin_substr(input_string, idx, idx)
         #Even expansion
-        max_even_len = get_max_palin_substr(input_string, idx, idx + 1)
+        even_palin, max_even_len = get_max_palin_substr(input_string, idx, idx + 1)
 
-        max_length = max(max_length, max_even_len, max_odd_len)
+        # Return the string =>
+        if len(longest_palin) < len(odd_palin): longest_palin = odd_palin
+        elif len(longest_palin) < len(even_palin): longest_palin = even_palin
 
-    return max_length
+        # Return the max length =>    
+        # max_length = max(max_length, max_even_len, max_odd_len)
+    
+    # return max_length
+    return longest_palin
 
 def get_max_palin_substr(input_string, left_idx, right_idx):
 
     while left_idx >= 0 and right_idx < len(input_string) and input_string[left_idx] == input_string[right_idx]:
         left_idx -= 1
         right_idx += 1
-
-    return right_idx - left_idx - 1
+    
+    # Return palin, length
+    return input_string[left_idx + 1 : right_idx], right_idx - left_idx - 1
 
 
 class TestLongestPalindromicSubString(unittest.TestCase):
     def test_empty_string(self):
-        self.assertEqual(longest_palindromic_substring(""), 0)
+        # self.assertEqual(longest_palindromic_substring(""), 0)
+        self.assertEqual(longest_palindromic_substring(""), '')
     
     def test_one_char_string(self):
-        self.assertEqual(longest_palindromic_substring("a"), 1)
+        # self.assertEqual(longest_palindromic_substring("a"), 1)
+        self.assertEqual(longest_palindromic_substring("a"), 'a')
 
     def test_long_string(self):
-        self.assertEqual(longest_palindromic_substring("banana"), 5)
-
-
-# def main():
-#     print(longest_palindromic_substring("ababc"))
+        # self.assertEqual(longest_palindromic_substring("banana"), 5)
+        self.assertEqual(longest_palindromic_substring("banana"), "anana")
 
 if __name__ == "__main__":unittest.main()
