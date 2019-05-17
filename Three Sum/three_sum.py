f048c21416e5f4d451195a107473deb95eda6935
Ishan Gote <goteishan@gmail.com>
2019-05-17 13:46:11 -0700

    done done commit

--- Three Sum/three_sum.py
+++ Three Sum/three_sum.py
@@ -1,58 +1,36 @@
-# Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? F
-# ind all unique triplets in the array which gives the sum of zero.
-#Note: The solution set must not contain duplicate triplets.
-
-"""
-[-1, 0, 1, 2, -1, -4]
-  
-Sort: 
-
-[-4, -1, -1, 0, 1, 2]
-  ^   l 
-          r
-
-[[-1, -1, 2], [-1, 0, 1]]
-
-
-[
-  [-1, 0, 1],
-  [-1, -1, 2]
-]
-
-
-"""
+# Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
+# Find all unique triplets in the array which gives the sum of zero.
+# Note: The solution set must not contain duplicate triplets.
 
 import unittest
 def three_sum(nums):
-    result = []
     nums = sorted(nums)
+    solutions = []
     for idx in range(0, len(nums) - 3):
-        #No duplicates condition =>
-        if idx == 0 or nums[idx] > nums[idx - 1]:
-            left, right = idx + 1, len(nums) - 1
+        if nums[idx] > 0: break
+        if idx > 0 and nums[idx] == nums[idx - 1]: continue
+        left, right = idx + 1, len(nums) - 1
 
-            while left < right:
-                if nums[left] + nums[right] + nums[idx] == 0: 
-                    result.append([nums[idx], nums[left], nums[right]])
-                    #We still have to increment/decrement left/right
+        while left < right:
+            sum = nums[idx] + nums[left] + nums[right]
+            if sum == 0:
+                solutions.append([nums[idx], nums[left], nums[right]])
 
-                if nums[left] + nums[right] + nums[idx] < 0:
-                    curr_left = left
-                    #Not to consider same values of left
-                    while(nums[left] == nums[curr_left] and left < right):
-                        left += 1
+                left_value = nums[left]
+                while left < len(nums) and nums[left] == left_value: left += 1
 
-                else:
-                    curr_left = left
-                    #Not to consider same values of left
-                    while(nums[left] == nums[curr_left] and left < right):
-                        right -= 1
+                right_value = nums[right]
+                while right > left and nums[right] == right_value: right -= 1
 
-    return result
+            elif sum < 0: left += 1
+            else: right -= 1
+
+    return solutions
 
 class TestThreeSum(unittest.TestCase):
     def test_generic_example(self):
-        self.assertEqual(three_sum([-1, 0, 1, 2, -1, -4]), [[-1, -1, 2]], [-1, 0, 1])
+        self.assertEqual(three_sum([-1, 0, 1, 2, -1, -4]), [[-1, -1, 2], [-1, 0, 1]])
+        self.assertEqual(three_sum([0, -1, 2, -3, 1]), [[-3, 1, 2], [-1, 0, 1]])
 
     def test_all_zero_input(self):
         self.assertEqual(three_sum([0, 0, 0, 0, 0]), [[0, 0, 0]])
@@ -64,5 +42,3 @@ class TestThreeSum(unittest.TestCase):
         self.assertEqual(three_sum([2, 3, 4, 5, 6, 7]), [])
 
 if __name__ == "__main__": unittest.main()
-
-
