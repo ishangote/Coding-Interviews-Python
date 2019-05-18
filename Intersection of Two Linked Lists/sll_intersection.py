367579bbc8eaf44446f6141d5360e9af51a71a60
Ishan Gote <goteishan@gmail.com>
2019-05-18 10:26:50 -0700

    done done commit

--- Intersection of Two Linked Lists/sll_intersection.py
+++ Intersection of Two Linked Lists/sll_intersection.py
@@ -12,9 +12,6 @@ list1 = [4, 1, 8, 6, 5]
          *            
 list2 = [0, 2, 3, 8, 6, 5]
          ^
-
-
-       
 """
 
 class SLL_Node:
@@ -48,21 +45,43 @@ def sll_intersection_two_pointers(l1, l2):
 
 import unittest
 class TestSLLIntersection(unittest.TestCase):
-        def test_sll_intersection(self):
-            l1 = SLL_Node(4)
-            l1.next = SLL_Node(1)
-            l1.next.next = SLL_Node(8)
-            l1.next.next.next = SLL_Node(4)
-            l1.next.next.next.next = SLL_Node(5)
 
-            l2 = SLL_Node(5)
-            l2.next = SLL_Node(0)
-            l2.next.next = SLL_Node(1)
-            l2.next.next.next = l1.next.next
-            l2.next.next.next.next = l1.next.next.next
-            l2.next.next.next.next.next = l1.next.next.next.next
+    def test_l1_l2_none(self):
+        l1 = SLL_Node(4)
+        l2 = SLL_Node(5)
 
-            self.assertEqual(sll_intersection_hm(l1, l2), l1.next.next)
-            self.assertEqual(sll_intersection_two_pointers(l1, l2), l1.next.next)
+        self.assertEqual(sll_intersection_hm(None, l2), None)
+        self.assertEqual(sll_intersection_hm(l1, None), None)
+        self.assertEqual(sll_intersection_hm(None, None), None)
+
+        self.assertEqual(sll_intersection_two_pointers(None, l2), None)
+        self.assertEqual(sll_intersection_two_pointers(l1, None), None)
+        self.assertEqual(sll_intersection_two_pointers(None, None), None)
+
+    def test_no_intersection(self):
+        l1 = SLL_Node(4)
+        l1.next = SLL_Node(1)
+        l2 = SLL_Node(5)
+        l2.next = SLL_Node(0)
+
+        self.assertEqual(sll_intersection_hm(l1, l2), None)
+        self.assertEqual(sll_intersection_two_pointers(l1, l2), None)
+    
+    def test_sll_intersection(self):
+        l1 = SLL_Node(4)
+        l1.next = SLL_Node(1)
+        l1.next.next = SLL_Node(8)
+        l1.next.next.next = SLL_Node(4)
+        l1.next.next.next.next = SLL_Node(5)
+
+        l2 = SLL_Node(5)
+        l2.next = SLL_Node(0)
+        l2.next.next = SLL_Node(1)
+        l2.next.next.next = l1.next.next
+        l2.next.next.next.next = l1.next.next.next
+        l2.next.next.next.next.next = l1.next.next.next.next
+
+        self.assertEqual(sll_intersection_hm(l1, l2), l1.next.next)
+        self.assertEqual(sll_intersection_two_pointers(l1, l2), l1.next.next)
 
 if __name__ == "__main__": unittest.main()
