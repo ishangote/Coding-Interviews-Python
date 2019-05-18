62499cb64da3a1a2610cf753037f9d823722709d
Ishan Gote <goteishan@gmail.com>
2019-05-17 19:45:10 -0700

    done commit

---
+++ Intersection of Two Linked Lists/sll_intersection.py
@@ -0,0 +1,68 @@
+# Write a program to find the node at which the intersection of two singly linked lists begins.
+
+"""
+Approach1:
+HashTable -> 
+
+
+Approach 2:
+Two pointers => 
+
+list1 = [4, 1, 8, 6, 5]
+         *            
+list2 = [0, 2, 3, 8, 6, 5]
+         ^
+
+
+       
+"""
+
+class SLL_Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def sll_intersection_hm(l1, l2):
+    hs = set()
+
+    while l1:
+        hs.add(l1)
+        l1 = l1.next
+    
+    while l2:
+        if l2 in hs: return l2
+        else: l2 = l2.next
+
+    return None
+
+def sll_intersection_two_pointers(l1, l2):
+    pointer1, pointer2 = l1, l2
+    
+    while pointer1 != pointer2:
+        pointer1 = pointer1.next if pointer1 else l2
+        pointer2 = pointer2.next if pointer2 else l1
+
+    return pointer1
+
+
+import unittest
+class TestSLLIntersection(unittest.TestCase):
+        def test_sll_intersection(self):
+            l1 = SLL_Node(4)
+            l1.next = SLL_Node(1)
+            l1.next.next = SLL_Node(8)
+            l1.next.next.next = SLL_Node(4)
+            l1.next.next.next.next = SLL_Node(5)
+
+            l2 = SLL_Node(5)
+            l2.next = SLL_Node(0)
+            l2.next.next = SLL_Node(1)
+            l2.next.next.next = l1.next.next
+            l2.next.next.next.next = l1.next.next.next
+            l2.next.next.next.next.next = l1.next.next.next.next
+
+            self.assertEqual(sll_intersection_hm(l1, l2), l1.next.next)
+            self.assertEqual(sll_intersection_two_pointers(l1, l2), l1.next.next)
+
+if __name__ == "__main__": unittest.main()
