b41ebb3f19d8f330c8af32f28203a33599962f0d
Ishan Gote <goteishan@gmail.com>
2019-05-26 15:40:35 -0700

    Fixed issue with modification of the board

--- Word Search/word_search.py
+++ Word Search/word_search.py
@@ -20,8 +20,7 @@ SEE
 """
 
 def word_search(board, word):
-  if len(word) == 0: return True
-  if len(board) == 0 or len(board[0]) == 0: return False
+  if len(word) == 0 or len(board) == 0: return False
 
   for i in range(len(board)):
     for j in range(len(board[0])):
@@ -34,24 +33,31 @@ def word_search_helper(board, word, row, col):
 
   if 0 <= row < len(board) and 0 <= col < len(board[0]):
     if board[row][col] == word[0]:
+      temp_value = board[row][col]
       board[row][col] = '#'
-      return word_search_helper(board, word[1:], row + 1, col) or word_search_helper(board, word[1:], row - 1, col) or word_search_helper(board, word[1:], row, col + 1) or word_search_helper(board, word[1:], row, col - 1)
-    else: return False
 
+      return_flag =  word_search_helper(board, word[1:], row + 1, col) or word_search_helper(board, word[1:], row - 1, col) or word_search_helper(board, word[1:], row, col + 1) or word_search_helper(board, word[1:], row, col - 1)
+
+      board[row][col] = temp_value
+      return return_flag
+      
   return False
 
 import unittest
 class TestWordSearch(unittest.TestCase):
   def test_invlaid_input(self):
     self.assertEqual(word_search([], "SEE"), False)
-    self.assertEqual(word_search([['A','B','C','E'], ['S','F','C','S'], ['A','D','E','E']], ""), True)
+    self.assertEqual(word_search([['A','B','C','E'], ['S','F','C','S'], ['A','D','E','E']], ""), False)
 
   def test_word_search_word_found(self):
     self.assertEqual(word_search([['A','B','C','E'], ['S','F','C','S'], ['A','D','E','E']], "SEE"), True)
     self.assertEqual(word_search([['A','B','C','E'], ['S','F','C','S'], ['A','D','E','E']], "ABCCED"), True)
-
+    self.assertEqual(word_search([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED"), True)
+    self.assertEqual(word_search([["C","A","A"],["A","A","A"],["B","C","D"]], "AAB"), True)
+    
   def test_word_search_word_not_found(self):
     self.assertEqual(word_search([['A','B','C','E'], ['S','F','C','S'], ['A','D','E','E']], "ABCB"), False)
     self.assertEqual(word_search([['A','B','C','E'], ['S','F','C','S'], ['A','D','E','E']], "FEE"), False)
+    self.assertEqual(word_search([["A", "A"]], "AAA"), False)
 
 if __name__ == "__main__": unittest.main()
