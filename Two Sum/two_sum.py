15351e401e7fa1a1bd38c91125416302f92a5507
Ishan Gote <goteishan@gmail.com>
2019-05-26 12:50:51 -0700

    added a negative nums testcase

--- Two Sum/two_sum.py
+++ Two Sum/two_sum.py
@@ -21,7 +21,10 @@ class TestTwoSumUnitTest(unittest.TestCase):
     def two_sum_no_pair(self):
         self.assertEqual(two_sum([1, 3, 7, 5], 3), [])
 
-    def two_sum_generic(self):
+    def test_two_sum_negative_nums(self):
+        self.assertEqual(two_sum([-2, 4, 1, -3, 3], 1), [1, 3])
+
+    def two_sum_generic(self):    
         self.assertEqual(two_sum([2, 7, 11, 15], 9), [0, 1])
 
 if __name__ == "__main__": unittest.main()
