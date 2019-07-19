fc0b1d7a011c3ff0211babd7894fc5a096d2b0df
Ishan Gote <goteishan@gmail.com>
2019-07-19 14:58:11 -0400

    initial commit

--- Populating Next Right Pointers in Each Node/populate_next_right.py
+++ Populating Next Right Pointers in Each Node/populate_next_right.py
@@ -31,11 +31,45 @@ class BinaryTreeNode:
         self.right = None
         self.next = None
 
+#Level order traversal
 from collections import deque
 def populate_next_right(root):
     if not root: return None
 
     dq = deque([root])
     while dq:
+      num_nodes = len(dq)
+      next_node = None
+      for itr in range(num_nodes):
         node = dq.pop()
-        
+        node.next = next_node
+        next_node = node
+        if node.right: dq.appendleft(node.right)
+        if node.left: dq.appendleft(node.left)
+
+import unittest
+class TestPopulateNextRightPerfectBinaryTree(unittest.TestCase):
+  root = BinaryTreeNode(1)
+  root.left = BinaryTreeNode(2)
+  root.right = BinaryTreeNode(3)
+  root.left.left = BinaryTreeNode(4)
+  root.left.right = BinaryTreeNode(5)
+  root.right.left = BinaryTreeNode(6)
+  root.right.right = BinaryTreeNode(7)
+
+  def print_all_next_in_level_order(self):
+    result = []
+    if not self.root: return None
+    dq = deque([self.root])
+    while dq:
+      node = dq.pop()
+      if node.next: result.append(node.next.val)
+      else: result.append(None)
+      if node.right: dq.appendleft(node.right)
+      if node.left: dq.appendleft(node.left)
+    return result
+
+  def test_populate_next_right(self):
+    populate_next_right(self.root)
+    result = self.print_all_next_in_level_order()
+    self.assertEqual(result, [None, 3, None, 5, 6, 7, None])
