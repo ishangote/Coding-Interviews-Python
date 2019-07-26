0572f1543105ac1596fba4eadbd9b2385aeaccc2
Ishan Gote <goteishan@gmail.com>
2019-07-25 22:50:07 -0400

    done done commit

--- Combination Sum Problems/combination_sum_ii.py
+++ Combination Sum Problems/combination_sum_ii.py
@@ -24,3 +24,28 @@ A solution set is:
   [5]
 ]
 """
+
+def combination_sum_ii(nums, target):
+  result = []
+  nums.sort()
+  backtrack(result, [], nums, target, 0)
+  return result
+
+import copy
+def backtrack(result, combination, nums, remain, start):
+  if remain < 0: return
+  elif remain == 0: result.append(copy.deepcopy(combination))
+  else:
+    for idx in range(start, len(nums)):
+      if idx > start and nums[idx] == nums[idx - 1]: continue #to skip duplicates
+      combination.append(nums[idx])
+      backtrack(result, combination, nums, remain - nums[idx], idx + 1)
+      combination.pop(len(combination) - 1)
+
+import unittest
+class TestCombinationSum(unittest.TestCase):
+    def test_combination_sum(self):
+        self.assertEqual(sorted(combination_sum_ii([10,1,2,7,6,1,5], 8)), sorted([[1, 7], [1, 2, 5], [2, 6], [1, 1, 6]]))
+        self.assertEqual(sorted(combination_sum_ii([2,5,2,1,2], 5)), sorted([[1,2,2], [5]]))
+
+if __name__ == "__main__": unittest.main()
