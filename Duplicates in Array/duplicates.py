076e7d3cce793588039f9196d7c2cdaa45bbdf3a
Ishan Gote <goteishan@gmail.com>
2019-08-06 01:33:28 -0400

    done done commit

--- Duplicates in Array/duplicates.py
+++ Duplicates in Array/duplicates.py
@@ -1,11 +1,11 @@
-# Given an array of n elements which contains elements from 0 to n-1
+# Given an array of n elements which contains elements from 1 to n-1
 # with any of these numbers appearing any number of times. Find these repeating numbers in O(n) and using only constant memory space.
 # For example, let n be 7 and array be {1, 2, 3, 1, 3, 6, 6}, the answer should be 1, 3 and 6.
 
 
 
 """
-# [IMPORTANT] -> positive and not greater than length of array
+# [IMPORTANT] -> positive and not greater than length of array and also greater than zero
 
 The idea is we do a linear pass using the input array itself as a hash to store which numbers have been seen before. 
 We do this by making elements at certain indexes negative. See the full explanation here
@@ -64,7 +64,7 @@ import unittest
 class TestFindDuplicatesInArray(unittest.TestCase):
     def test_no_duplicates_input(self):
         self.assertEqual(find_all_duplicates([1, 4, 2, 3]), [])
-
+        
     def test_positive_input(self):
         self.assertEqual(find_all_duplicates([1, 2, 3, 1, 3, 6, 6]), [1, 3, 6])
 
