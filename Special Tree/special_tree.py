03ea07294acd6a33228f139619eccea1214ca297
Ishan Gote <goteishan@gmail.com>
2019-08-13 16:42:23 -0400

    added one function dfs

--- Special Tree/special_tree.py
+++ Special Tree/special_tree.py
@@ -10,37 +10,53 @@ Return true if the given tree is univalued.
 """
 import unittest
 class SpecialTreeNode:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, val):
+        self.val = val
         self.left = None
         self.right = None
 
 def is_special_tree(root):
-    if root == None: return False
-    return dfs(root, root.data)
+    if root == None: return True
+    return dfs(root, root.val)
     
 def dfs(node, val):
     if node == None: return True
-    if node.data != val: return False
+    if node.val != val: return False
     return dfs(node.left, val) and dfs(node.right, val)
+
+#-------------One function-------------#
+
+def is_special_dfs(root):
+    if not root: return True
+    
+    if root.right:
+        if root.val != root.right.val: return False
+        
+    if root.left:
+        if root.val != root.left.val: return False
+    
+    return is_special_dfs(root.right) and is_special_dfs(root.left)
 
 #-------------TEST-------------#
 
 class TestIsSpecialTree(unittest.TestCase):
     def test_root_none(self):
-        self.assertEqual(is_special_tree(None), False)
+        self.assertEqual(is_special_tree(None), True)
+        self.assertEqual(is_special_dfs(None), True)
 
     def test_no_left_root(self):
         a = SpecialTreeNode(1)
         b = SpecialTreeNode(1)
         a.right = b
         self.assertEqual(is_special_tree(a), True)
+        self.assertEqual(is_special_dfs(a), True)
     
     def test_no_right_root(self):
         a = SpecialTreeNode(1)
         b = SpecialTreeNode(1)
         a.left = b
         self.assertEqual(is_special_tree(a), True)
+        self.assertEqual(is_special_dfs(a), True)
 
     def test_special_tree(self):
         a = SpecialTreeNode(1)
@@ -70,6 +86,8 @@ class TestIsSpecialTree(unittest.TestCase):
                    h
         """
         self.assertEqual(is_special_tree(a), True)
+        self.assertEqual(is_special_dfs(a), True)
+
 
     def test_not_special_tree(self):
         a = SpecialTreeNode(1)
@@ -93,6 +111,7 @@ class TestIsSpecialTree(unittest.TestCase):
         e.left = h
 
         self.assertEqual(is_special_tree(a), False)
+        self.assertEqual(is_special_dfs(a), False)
 
     def test_root_value_different_node(self):
         a = SpecialTreeNode(2)
@@ -116,15 +135,6 @@ class TestIsSpecialTree(unittest.TestCase):
         e.left = h
 
         self.assertEqual(is_special_tree(a), False)
-
-# def main():
-#     a = SpecialTreeNode(1)
-#     b = SpecialTreeNode(1)
-#     c = SpecialTreeNode(2)
-
-#     a.left = b
-#     a.right = c
-
-#     print (is_special_tree(a))
+        self.assertEqual(is_special_dfs(a), False)
 
 if __name__ == '__main__': unittest.main()
