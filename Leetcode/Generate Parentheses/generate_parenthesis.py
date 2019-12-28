f5a389fa81c68bef648d8dd65fa477a23310430f
Ishan Gote <goteishan@gmail.com>
2019-12-28 18:32:29 +0530

    explanantion provided

--- Leetcode/Generate Parentheses/generate_parenthesis.py
+++ Leetcode/Generate Parentheses/generate_parenthesis.py
@@ -1,11 +1,27 @@
 
 """
+Solution: https://www.youtube.com/watch?v=sz1qaKt0KGQ&t=1s
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+
 n = 1 ()
 n = 2 (()), ()()
 n = 3 ((())), (()()), , ()(()), (())(), ()()()
 
 -> length of each is 2 * n -> n open n close
 
+Our Choice -> Open a bracket or close a bracket
+Our Constraints -> 1. cannot close until we open. 2. n count of open brackets
+Our Goal -> n*2 is length of any correct answer n = 1 () -> 2n length
 
 
 """
