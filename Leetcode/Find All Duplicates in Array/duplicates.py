66572d5158386f1ac8f04af0d4c67658c82eb1ff
Ishan Gote <goteishan@gmail.com>
2020-01-12 12:47:48 +0530

    added generic solution for n times repeat

--- Leetcode/Find All Duplicates in Array/duplicates.py
+++ Leetcode/Find All Duplicates in Array/duplicates.py
@@ -1,11 +1,17 @@
-# Given an array of n elements which contains elements from 1 to n-1
-# with any of these numbers appearing any number of times. Find these repeating numbers in O(n) and using only constant memory space.
-# For example, let n be 7 and array be {1, 2, 3, 1, 3, 6, 6}, the answer should be 1, 3 and 6.
-
+# Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+# Find all the elements that appear twice in this array.
+# Could you do it without extra space and in O(n) runtime?
 
 
 """
 # [IMPORTANT] -> positive and not greater than length of array and also greater than zero
+
+Example:
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[2,3]
 
 The idea is we do a linear pass using the input array itself as a hash to store which numbers have been seen before. 
 We do this by making elements at certain indexes negative. See the full explanation here
@@ -52,13 +58,14 @@ nums[abs(x) - 1] = -6 < 0 => res.append(abs(x)) = [1, 3, 6]
 
 def find_all_duplicates(nums):
     if not nums: return None
-    ans = []
+    #solved it for n number of times repeat. Hence use set.
+    ans = set()
     
     for x in nums:
         if nums[abs(x) - 1] > 0: nums[abs(x) - 1] *= -1
-        else: ans.append(abs(x))
+        else: ans.add(abs(x))
 
-    return ans
+    return list(ans)
 
 import unittest
 class TestFindDuplicatesInArray(unittest.TestCase):
