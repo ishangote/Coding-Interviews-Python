da57e2876035081abf564ce684efb791a042aeab
Ishan Gote <goteishan@gmail.com>
2020-03-17 14:24:11 -0400

    done done commit

---
+++ Leetcode/Pow(x,n)/pow_x_n.py
@@ -0,0 +1,44 @@
+"""
+SOLUTION: https://www.youtube.com/watch?v=wAyrtLAeWvI
+
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+Input: 2.00000, 10
+Output: 1024.00000
+
+Example 2:
+Input: 2.10000, 3
+Output: 9.26100
+
+Example 3:
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+Note:
+
+-100.0 < x < 100.0
+n is a 32-bit signed integer, within the range [−231, 231 − 1]
+"""
+def pow_util(x, n):
+    if n == 0: return 1
+
+    half = pow_util(x, n // 2)
+    if n % 2 == 0: return half * half
+    else: return half * half * x
+
+def pow_x_n(x, n):
+    if n < 0:
+        x = 1 / x
+        n *= -1
+    
+    return pow_util(x, n)
+
+import unittest
+class TestPowXN(unittest.TestCase):
+    def test_pow_x_n(self):
+        self.assertEqual(round(pow_x_n(2.00000, 10), 5), 1024.00000)
+        self.assertEqual(round(pow_x_n(2.10000, 3), 5), 9.26100)
+        self.assertEqual(round(pow_x_n(2.00000, -2), 5), 0.25000)
+
+if __name__ == "__main__": unittest.main()
