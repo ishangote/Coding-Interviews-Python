71a04ef8ae33a565e67e7eae85018b0f62cca3a7
Ishan Gote <goteishan@gmail.com>
2020-03-29 20:43:59 -0400

    dynamic programming approach finalized

--- Leetcode/Trapping Rain Water/trapping_water.py
+++ Leetcode/Trapping Rain Water/trapping_water.py
@@ -17,7 +17,6 @@ right max for each i         =>         3   3   3   3   3   3   3   2   2   2   
 Goal: To find leftmax, rightmax =>
 
 Approach 1: Brute Force
-
 For each i move towards the left again to find the left_max and then to the right to find the right max
 Time: O(n^2)
 Space: O(1)
@@ -29,42 +28,32 @@ One reverse iteration to calculate the right max array
 
 Time: O(n + n) = O(n)
 Space: O(n + n) = O(n)
-----------------------------------------
-Approach 3: Stacks
-Instead of storing the largest bar upto an index as in Approach 2, 
-we can use stack to keep track of the bars that are bounded by longer bars and hence, may store water.
-We add the index of the bar to the stack if bar is smaller than or equal to the bar at top of stack, 
-which means that the current bar is bounded by the previous bar in the stack. 
-If we found a bar longer than that at the top, we are sure that the bar at the top of the stack is
-bounded by the current bar and a previous bar in the stack, 
-hence, we can pop it and add resulting trapped water to ans.
-
-Water is trapped when an increasing height appears.
-
-Time: O(n)
-Space: O(n)
 """
 
-def trapping_water_stack(heights):
+def trapping_water(heights):
     if not heights: return 0
-    ans, stack = 0, []
+    left_max = [0 for i in range(len(heights))]
+    right_max = [0 for i in range(len(heights))]
 
-    for i, v in enumerate(heights):
-        while stack and heights[stack[-1]] < v:
-            prev_bound = heights[stack.pop()]
-            if not stack: break
-            left_max = stack[-1]
-            h = min(heights[left_max], v) - prev_bound
-            w = i - left_max - 1
-            ans += h * w
+    #Populate left max
+    left_max[0] = heights[0]
+    for i in range(1, len(left_max)):
+        left_max[i] = max(heights[i], left_max[i - 1])
 
-        stack.append(i)
+    #Populate right max
+    right_max[-1] = heights[-1]
+    for i in range(len(right_max) - 2, -1, -1):
+        right_max[i] = max(heights[i], right_max[i + 1])
+
+    ans = 0
+    for i in range(len(heights)):
+        ans += min(left_max[i], right_max[i]) - heights[i]
 
     return ans
 
 import unittest
 class TestTrappingRainWater(unittest.TestCase):
     def test_generic(self):
-        self.assertEqual(trapping_water_stack([0, 1, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 5)
+        self.assertEqual(trapping_water([0, 1, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 5)
 
 if __name__ == "__main__": unittest.main()
