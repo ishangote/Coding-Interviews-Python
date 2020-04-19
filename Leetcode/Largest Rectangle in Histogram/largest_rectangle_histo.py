e019c45f50b215fde33e51681978204abc8495a8
Ishan Gote <goteishan@gmail.com>
2020-04-18 21:23:42 -0400

    done done commit

--- Leetcode/Largest Rectangle in Histogram/largest_rectangle_histo.py
+++ Leetcode/Largest Rectangle in Histogram/largest_rectangle_histo.py
@@ -24,8 +24,60 @@ Space Complexity: O(1)
 
 Approach 3: Stack:
 
-Case 1:
+Case 1: reverse sorted =>
 
+heights
+ 0  1  2  3  4
+[5, 4, 3, 2, 1]  Area =>
+             ^
+[5  8  9  8  5]
+
+
+Case 2: sorted =>
+
+heights
+ 0  1  2  3  4
+[1, 2, 3, 3, 5]  Area =>
+             ^                                                                  h   w
+When we reach index 2 (say) we need to know previous heights to calculate area (1 * 3 = 3) => Stack
+
+heights
+ 0  1  2  3  4 5
+[1, 2, 3, 3, 5]   Area =>
+               ^
+
+stack = [0, 1, 2, 3, 4] (INDEX)
+curr_idx = 5 
+
+pop stack =>
+curr_stack = [0, 1, 2, 3, 4]
+pop = 4 (5), 
+new_stack_top = 3,
+A = pop * (curr_idx - new_stack_top - 1) = 5 * (5 - 3 - 1) = 5
+
+curr_stack = [0, 1, 2, 3]
+pop = 3 (3), 
+new_stack_top = 2,
+A = pop * (curr_idx - new_stack_top - 1) = 3 * (5 - 2 - 1) = 6
+
+...
+
+
+Case 3: Mixed
+
+Algorithm:
+1. Iterate (idx) over array:
+2. if heights[idx] < heights[stack.top()]:
+        Calculate Area:
+            height = heights[stack.pop()]
+            width = idx - stack.top() - 1
+
+3. stack.push(idx)
+
+NOTE: In cases when stack has only one element i.e. -1 then stack[-1] = -1 and so heights[stack[-1]] = heights[-1] = 0 
+because we appended 0 to heights and 0 is smaller than all other heights, 
+so popping will stop there and -1 will never be popped out of the stack and so 
+this works as a stopping criterion (instead of doing something like this if stack).
 
 """
 def largest_rectangle(heights):
