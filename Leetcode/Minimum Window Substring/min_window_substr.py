36cb778a5f256819bb13e46a47e14654f502002c
Ishan Gote <goteishan@gmail.com>
2020-06-10 14:03:53 -0400

    cleaner code updated

--- Leetcode/Minimum Window Substring/min_window_substr.py
+++ Leetcode/Minimum Window Substring/min_window_substr.py
@@ -4,7 +4,7 @@ Substring => Contiguous
 ms = "aaat"
 ch= "t"
 
-Approach 1: Brute Force
+Approach 1: Brute Force O(n^2)
 
 0 1 2 3
 a a a t
@@ -24,45 +24,55 @@ left = 0 => a
 
 keep check of the best length substring
 
-Approach 2:
+Approach 2: Sliding Window
+t = abc
+s =
+0 1 2 3 4 5 6 7 8 9 10
+a b d b a n b c d d d
 
-how many times to search substring in BF?
+unique_chars_count = 0
+chars_count_map = {a: 0, b: 0, c: 0}
+l, r = 0, 0
+st, end = 0, inf
 
-a z j s k f z t s, sz
-l
-r
+s =
+0 1 2 3 4 5 6 7 8 9 10
+a b d b a n b c d d d
 
-a z j s k f z t s, sz
-l
-      r
+unique_chars_count = 1
+chars_count_map = {a: 1, b: 0, c: 0}
+l, r = 0, 0
+st, end = 0, inf
 
-a z j s k f z t s, sz           Best Soln: zjs
-  l
-      r
+s =
+0 1 2 3 4 5 6 7 8 9 10
+a b d b a n b c d d d
+l r
+unique_chars_count = 2
+chars_count_map = {a: 1, b: 1, c: 0}
+l, r = 0, 1
+st, end = 0, inf
 
-a z j s k f z t s, sz
-    l
-      r
+s =
+0 1 2 3 4 5 6 7 8 9 10
+a b d b a n b c d d d
+l   r
+unique_chars_count = 2
+chars_count_map = {a: 1, b: 1, c: 0}
+l, r = 0, 1
+st, end = 0, inf
 
-a z j s k f z t s, sz
-    l
-            r
+s =
+0 1 2 3 4 5 6 7 8 9 10
+a b d b a n b c d d d
+l     r
+unique_chars_count = 2
+chars_count_map = {a: 1, b: 2, c: 0}
+l, r = 0, 1
+st, end = 0, inf
 
-a z j s k f z t s, sz
-        l
-            r
+...
 
-a z j s k f z t s, sz
-        l
-                r
-
-a z j s k f z t s, sz       Best soln too
-            l
-                r
-
-a z j s k f z t s, sz
-              l
-                  r
 """
 #Example of Counter
 # >>> a = Counter(t)
@@ -78,31 +88,30 @@ a z j s k f z t s, sz
 # Counter({'a': 2, 'b': 1, 'c': 1, 'd': 1, 'e': 1, 'k': -1})
 # >>> a['n'] > 0
 # False
+import sys
+def min_window_substring(s, t):
+    unique_chars_count = 0
+    st, end = 0, sys.maxsize
+    left = 0
 
-from collections import Counter
-def min_window_substring(s, t):    
-    hm = Counter(t)
-    missing = len(t)    #total number of characters to match
-    start, end = 0, 0   # for best solution
-    left = 0    #keep moving right right
-    
-    for right, ch in enumerate(s, 1):   #iterate right from index 1
-        if hm[ch] > 0:
-            missing -= 1
-        hm[ch] -= 1
-        
-        if missing == 0: #all characters present
-            while left < right and hm[s[left]] < 0:
-                hm[s[left]] += 1
-                left += 1
-            hm[s[left]] += 1
-            missing += 1
+    chars_count_map = {}
+    for ch in t:
+        chars_count_map[ch] = 0
 
-            if end == 0 or right - left < end - start:
-                start, end = left, right   # best solution
+    for right, right_val in enumerate(s):
+        if right_val not in chars_count_map: continue
+        if chars_count_map[right_val] == 0: unique_chars_count += 1
+        chars_count_map[right_val] += 1
+
+        while unique_chars_count == len(t):
+            if end - st > right - left: st, end = left, right
+            if s[left] in chars_count_map:
+                chars_count_map[s[left]] -= 1
+                if chars_count_map[s[left]] == 0: unique_chars_count -= 1
+
             left += 1
 
-    return s[start:end]
+    return s[st : end + 1] if end - st != sys.maxsize else ""
 
 import unittest
 class TestMinWindowSubstring(unittest.TestCase):
