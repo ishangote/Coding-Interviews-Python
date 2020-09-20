88992509679131aed81021a6718fd8aa0003649a
Ishan Gote <goteishan@gmail.com>
2020-09-20 12:46:32 -0700

    done done commit

---
+++ Leetcode/Linked List Cycle/has_cycle.py
@@ -0,0 +1,18 @@
+"""
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Follow up:
+
+Can you solve it using O(1) (i.e. constant) memory?
+"""
+
+def has_cycle(head):
+    slow = fast = head
+    while fast and fast.next and fast.next.next:
+        slow, fast = slow.next, fast.next.next
+        if slow == fast: return True
+    return False
